256721cb0f2b7c6966809327a6030d46a10fbec0
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-05-11 12:31:58 +0300

    Fix : authentication error

--- server/middlewares/auth.js
+++ server/middlewares/auth.js
@@ -1,15 +1,16 @@
 import "express-async-errors";
 import jwt from "jsonwebtoken";
 import ApiError from "../utils/ApiError.js";
+import User from "../models/User.js";
 
 export const authenticate = async (req, res, next) => {
   const token = req.cookies.token;
   if (!token)return next(new ApiError(401, "Authentication failed!"));
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
-  req.user = decoded;
-  next();
+  req.user = await User.findById(decoded.userId);
   if (!decoded) return next(new ApiError(401, "Invalid or expired token!"));
+  next();
 };
 export const authorizeAdmin = (req, res, next) => {
   if (req.user.role !== "admin") {

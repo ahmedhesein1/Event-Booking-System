ad151e255f3ff0e39cf75d3ffc2108738f82c0e5
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-05-11 12:31:23 +0300

    Fix: bug(type error)

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -4,14 +4,14 @@ import ApiError from "../utils/ApiError.js";
 export const getEvents = async (req, res, next) => {
   const events = await Event.find().populate("createdBy", "userName");
   res.status(200).json(events);
-  if (!event) next(new ApiError(404, "Events Not Found"));
+  if (!events) next(new ApiError(404, "Events Not Found"));
 };
 export const createEvent = async (req, res, next) => {
   const event = await Event.create({
-    ...req.Body,
+    ...req.body,
     createdBy: req.user._id,
   });
-  if (!req.body) next(new ApiError(400, "All fields are required"));
+  if (!req.body) return next(new ApiError(400, "All fields are required"));
   res.status(201).json(event);
 };
 export const updateEvent = async (req, res, next) => {

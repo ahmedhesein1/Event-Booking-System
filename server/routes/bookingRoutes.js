2ab42d81bc6718822adf87881110d82717110e7d
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-05-11 23:46:59 +0300

    Initial Commit

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -8,7 +8,8 @@ const router = express.Router();
 router.use(authenticate);
 
 router.get("/", bookingController.getBookings);
+router.get("/history", bookingController.getBookingsHistory);
 router.post("/:eventId", bookingController.createBooking); // eventId in URL params
-router.delete("/:id", bookingController.cancelBooking); // bookingId in URL params
+router.patch("/:id", bookingController.cancelBooking); // bookingId in URL params
 
 export default router;

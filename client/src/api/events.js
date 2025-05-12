cf285ba0a4b36ce78c9cda7962f2d3d9d9339574
ahmedhesein1 <ahmedhesin1790@gmail.com>
2025-05-12 12:33:56 +0300

    Initial Commit

---
+++ client/src/api/events.js
@@ -0,0 +1,36 @@
+import api from "./axios";
+
+export const getEvents = async () => {
+  try {
+    const response = await api.get("/events");
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const createEvent = async (eventData) => {
+  try {
+    const response = await api.post("/events", eventData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const updateEvent = async (id, eventData) => {
+  try {
+    const response = await api.patch(`/events/${id}`, eventData);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const deleteEvent = async (id) => {
+  try {
+    await api.delete(`/events/${id}`);
+  } catch (error) {
+    throw error;
+  }
+};
